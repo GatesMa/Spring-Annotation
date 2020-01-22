60c0e6fe758451a10e970be9ad77f4acb38af4b0
GatesMa <gatesma@foxmail.com>
2020-01-22 21:00:39 +0800

    Spring注解驱动开发

---
+++ src/main/java/com/gatesma/config/MainConfig.java
@@ -0,0 +1,15 @@
+package com.gatesma.config;
+
+/**
+ * Copyright (C), 2020
+ * FileName: MainConfig
+ * Author:   Marlon
+ * Email: gatesma@foxmail.com
+ * Date:     2020/1/22 21:00
+ * Description:
+ */
+public class MainConfig {
+
+     
+
+}
