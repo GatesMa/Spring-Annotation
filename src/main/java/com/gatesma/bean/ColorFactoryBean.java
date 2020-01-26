b4d27e033a0bc775cc33f780889960500cfa336d
GatesMa <gatesma@foxmail.com>
2020-01-26 18:38:50 +0800

    Spring注解驱动开发

--- src/main/java/com/gatesma/bean/ColorFactoryBean.java
+++ src/main/java/com/gatesma/bean/ColorFactoryBean.java
@@ -1,5 +1,7 @@
 package com.gatesma.bean;
 
+import org.springframework.beans.factory.FactoryBean;
+
 /**
  * Copyright (C), 2020
  * FileName: ColorFactoryBean
@@ -8,5 +10,27 @@ package com.gatesma.bean;
  * Date:     2020/1/26 17:42
  * Description:
  */
-public class ColorFactoryBean {
+//创建一个Spring定义的工厂Bean
+public class ColorFactoryBean implements FactoryBean<Color> {
+
+
+    @Override
+    public Color getObject() throws Exception {
+        System.out.println("ColorFactoryBean...getObject...");
+
+        return new Color();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return Color.class;
+    }
+
+    //是单例？
+    //true：这个bean是单实例，在容器中保存一份
+    //false：多实例，每次获取都会创建一个新的bean；
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
 }
