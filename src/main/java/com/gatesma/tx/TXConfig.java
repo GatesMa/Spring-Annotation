63c815ae6ac9e35ecced9a1bdcba8ab793f1fb88
GatesMa <gatesma@foxmail.com>
2020-02-02 11:38:24 +0800

    Spring注解驱动开发

--- src/main/java/com/gatesma/tx/TXConfig.java
+++ src/main/java/com/gatesma/tx/TXConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
@@ -63,6 +66,7 @@ import java.beans.PropertyVetoException;
  *
  */
 
+@EnableTransactionManagement
 @ComponentScan(value = {"com.gatesma.tx"})
 @Configuration
 public class TXConfig {
@@ -81,8 +85,15 @@ public class TXConfig {
 
     @Bean
     public JdbcTemplate jdbcTemplate() throws PropertyVetoException {
+        //Spring对@Configuration类会特殊处理；给容器中加组件的方法，多次调用都只是从容器中找组件
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource());
         return jdbcTemplate;
+    }
+
+    //注册事务管理器在容器中
+    @Bean
+    public PlatformTransactionManager platformTransactionManager() throws PropertyVetoException {
+        return new DataSourceTransactionManager(dataSource());
     }
 
 }
--- src/main/java/com/gatesma/tx/UserService.java
+++ src/main/java/com/gatesma/tx/UserService.java
@@ -2,6 +2,7 @@ package com.gatesma.tx;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Copyright (C), 2020
@@ -17,10 +18,12 @@ public class UserService {
 
     @Autowired
     private UserDAO userDAO;
-    
+
+    @Transactional
     public void insertUser() {
         userDAO.insert();
         System.out.println("插入完成!");
+        int i = 10 / 0;
     }
     
 }
